c181e50dfc8fc72f8cc1d8619d44e1c7f736d2ed
Jayesh <jayesh2598@gmail.com>
2020-09-25 14:41:30 +0530

    Start: Welcome message displayed

---
+++ LineComparison.java
@@ -0,0 +1,11 @@
+package com.capgemini.training;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Line Comparison Computation Program!");
+
+	}
+
+}
