2da09511df7573897a00c5f1355a2c77581158ff
Jayesh <jayesh2598@gmail.com>
2020-09-25 22:27:31 +0530

    Added: Created two lines and checked the equality of their length using equals method

--- LineComparison.java
+++ LineComparison.java
@@ -10,14 +10,23 @@ public class LineComparison {
 		System.out.println("Welcome to Line Comparison Computation Program!");
 		
 		//Generating random points
-		int[] point1=points();
+		int[] point1=points();			//Line 1 end points
 		int[] point2=points();
 		
-		//Calculating the distance between the two points
-		double line_length = LengthCalculator(point1, point2);
+		int[] point3=points();			//Line 2 end points
+		int[] point4=points();
+		
+		//Calculating the length of both lines
+		Double line1_length = new Double(LengthCalculator(point1, point2));
+		Double line2_length = new Double(LengthCalculator(point3, point4));
 		
 		System.out.println("Points: ("+ point1[0] + ","+point1[1] +  ") ; ("+ point2[0] + "," +point2[1]+")");
-		System.out.println(line_length);
+		System.out.println("Points: ("+ point3[0] + ","+point3[1] +  ") ; ("+ point4[0] + "," +point4[1]+")");
+		System.out.println("Line 1 length : "+line1_length);
+		System.out.println("Line 2 length : "+line2_length);
+		
+		//Checking equality using equals method
+		System.out.println("Are the two lines equal? "+ line1_length.equals(line2_length));
 	}
 	
 	public static int[] points() {
@@ -35,4 +44,6 @@ public class LineComparison {
 		double distance= Math.sqrt( Math.pow((point1[0]- point2[0]),2) + Math.pow((point1[1]- point2[1]),2) );
 		return distance;
 	}
+	
+	
 }
